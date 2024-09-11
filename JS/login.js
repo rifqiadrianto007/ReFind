62222049d72eb0d45668b3202827942c255393c1
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-09-11 13:40:06 +0700

    JS merge

---
+++ JS/login.js
@@ -0,0 +1,13 @@
+function handleLogin(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const pesanError = document.getElementById("pesan-error");
+
+  if (username !== "232410103098" && password !== "QWERTY12345") {
+    pesanError.textContent = "Username atau Password Anda Salah!";
+  } else {
+    window.location.href = "page-2.html";
+  }
+}
