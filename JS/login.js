dcafaeedcbb438894cb2bbaf78bf7ed99a7d4f59
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-09-15 09:52:37 +0700

    page-2 merge

--- JS/login.js
+++ JS/login.js
@@ -8,6 +8,6 @@ function handleLogin(event) {
   if (username !== "232410103098" && password !== "QWERTY12345") {
     pesanError.textContent = "Username atau Password Anda Salah!";
   } else {
-    window.location.href = "page-2.html";
+    window.location.href = "../Project ReFind/HTML/page-2.html";
   }
 }
